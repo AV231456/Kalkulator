22f62de205b5a82dfd0fd023ecdcfd15c346988c
Vlad <vkajmov883@gmail.com>
2024-09-13 13:48:37 +0400

    GOTOVO

---
+++ Полная/VlaKar/VlaKar/Program.cs
@@ -0,0 +1,26 @@
+﻿Console.WriteLine("Введите первое значение");
+int a = int.Parse(Console.ReadLine());
+Console.WriteLine("Введите второе значение");
+int b = int.Parse(Console.ReadLine());
+Console.WriteLine("Введите операцию");
+var o = Console.ReadLine();
+if (o == "+")
+{
+    Console.WriteLine($"Ответ {a + b}");
+}
+else if (o == "-")
+{
+    Console.WriteLine($"Ответ {a - b}");
+}
+else if (o == "*")
+{
+    Console.WriteLine($"Ответ {a * b}");
+}
+else if (o == "/")
+{
+    Console.WriteLine($"Ответ {a / b}");
+}
+else
+{
+    Console.WriteLine("Недопустимая операция!");
+}
