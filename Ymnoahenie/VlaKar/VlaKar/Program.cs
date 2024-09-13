5a16475808e9b30b67f4158ea346f509c702e875
Vlad <vkajmov883@gmail.com>
2024-09-13 14:22:28 +0400

    *

--- Ymnoahenie/VlaKar/VlaKar/Program.cs
+++ Ymnoahenie/VlaKar/VlaKar/Program.cs
@@ -1,7 +1,7 @@
 ﻿Console.WriteLine("Введите первое значение");
-int a = int.Parse(Console.ReadLine());
+double a = double.Parse(Console.ReadLine());
 Console.WriteLine("Введите второе значение");
-int b = int.Parse(Console.ReadLine());
+double  b = double.Parse(Console.ReadLine());
 Console.WriteLine("Введите операцию");
 var o = Console.ReadLine();
 if (o == "*")
